23d019863e803b5ed08c05945c1da5f94d2b9975
Joe Taglic <jctaglic@mit.edu>
2018-11-06 00:32:10 -0800

    Create Graph from Multiple Competitions

--- createSeasonGraph.py
+++ createSeasonGraph.py
@@ -1,7 +1,11 @@
 import util
+import snap
+# first approach: use multiple edges instead of weighted edges
+G = snap.TNEANet.New()
+name_map = {}
 
-seeding = util.getOrderedNameList('oct_nac_2017_seeding.csv')#["a", "b", "c", "d", "e"]
-results = util.getOrderedNameList('oct_nac_2017_results.csv')#["d", "c", "a", "b", "e"]
-G = util.createCompetitionGraph(seeding, results)
-print 'nodes: ', G.GetNodes()
-print 'edges: ', G.GetEdges()
+for month, year in [('oct','2017'),('april','2018'),('dec','2017'),('jan','2018'),('jul','2018')]:
+    seeding = util.getOrderedNameList(month+'_nac_'+year+'_seeding.csv')#["a", "b", "c", "d", "e"]
+    results = util.getOrderedNameList(month+'_nac_'+year+'_results.csv')#["d", "c", "a", "b", "e"]
+    util.createCompetitionGraph(seeding, results, G, name_map)
+    print 'nodes:', G.GetNodes(), ' edges:', G.GetEdges()
--- util.py
+++ util.py
@@ -14,7 +14,7 @@ def getOrderedNameList(path):
     return results
 
 
-def createCompetitionGraph(seeding, results):
+def createCompetitionGraph(seeding, results, G=None,  name_to_node_map=None, verbose=False):
     seeding_dict = {}
     results_dict = collections.defaultdict(lambda: sys.maxint)
     pos_to_name_dict = collections.defaultdict(lambda: "BYE") #mapping (actual value : representative value)
@@ -22,44 +22,46 @@ def createCompetitionGraph(seeding, results):
     match_win_list = []
 
     for place, name in enumerate(seeding):
-        seeding_dict[name] = place
-        pos_to_name_dict[place] = name
+        seeding_dict[name.lower()] = place
+        pos_to_name_dict[place] = name.lower()
 
     for place, name in enumerate(results):
-        results_dict[name] = place
+        results_dict[name.lower()] = place
     remaining_rounds = int(math.ceil(math.log(len(seeding),2)))
     for round_num in range(0, remaining_rounds):
         round_size = 2**(remaining_rounds - round_num)
-        print("Round size: {}").format(round_size)
+        if verbose:
+            print("Round size: {}").format(round_size)
         for expected_winner_num in range(0, round_size/2):
             expected_winner_name = pos_to_name_dict[expected_winner_num]
             expected_loser_num = round_size - (expected_winner_num + 1)
             expected_loser_name = pos_to_name_dict[expected_loser_num]
             if results_dict[expected_winner_name] > results_dict[expected_loser_name]:
-                print("\tUpset! {} just beat our expected_winner {}").format(expected_loser_name, expected_winner_name)
+                if verbose:
+                    print("\tUpset! {} just beat our expected_winner {}").format(expected_loser_name, expected_winner_name)
                 pos_to_name_dict[expected_winner_num] = expected_loser_name
                 pos_to_name_dict[expected_loser_num] = expected_winner_name
             else:
-                print("\tOur expected_winner {} just beat {}").format(expected_winner_name, expected_loser_name)
+                if verbose:
+                    print("\tOur expected_winner {} just beat {}").format(expected_winner_name, expected_loser_name)
             winner_name = pos_to_name_dict[expected_winner_num]
             loser_name = pos_to_name_dict[expected_loser_num]
             match_win_list.append((loser_name, winner_name))
-    node_to_name_map = {}
-    name_to_node_map = {}
+    if name_to_node_map is None:
+        name_to_node_map = {}
     i=0
-    G = snap.PNGraph.New()
+    if G is None:
+        G = snap.TNEANet.New()
     for loser, winner in match_win_list:
         if loser=="BYE":
             continue
         if loser not in name_to_node_map:
             nid = G.AddNode(-1)
-            node_to_name_map[nid] = loser
             name_to_node_map[loser] = nid
         if winner not in name_to_node_map:
             nid = G.AddNode(-1)
-            node_to_name_map[nid] = winner
             name_to_node_map[winner] = nid
         G.AddEdge(name_to_node_map[loser], name_to_node_map[winner])
-    return G
+    return G, name_to_node_map
 
 #createCompetitionGraph(seeding, results)
