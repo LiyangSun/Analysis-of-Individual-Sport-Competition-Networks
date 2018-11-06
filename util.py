15f0df037d53f6e5f1e397c69ab07b4199eb2be4
Joe Taglic <jctaglic@mit.edu>
2018-11-06 00:07:12 -0800

    Single Tournament SNAP Graph Construction

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import collections
 import sys
 import csv
 import fileinput
+import snap
 
 def getOrderedNameList(path):
     f = open(path)
@@ -18,6 +19,7 @@ def createCompetitionGraph(seeding, results):
     results_dict = collections.defaultdict(lambda: sys.maxint)
     pos_to_name_dict = collections.defaultdict(lambda: "BYE") #mapping (actual value : representative value)
     hierarchy_list = []
+    match_win_list = []
 
     for place, name in enumerate(seeding):
         seeding_dict[name] = place
@@ -39,5 +41,23 @@ def createCompetitionGraph(seeding, results):
                 pos_to_name_dict[expected_loser_num] = expected_winner_name
             else:
                 print("\tOur expected_winner {} just beat {}").format(expected_winner_name, expected_loser_name)
+            winner_name = pos_to_name_dict[expected_winner_num]
+            loser_name = pos_to_name_dict[expected_loser_num]
+            match_win_list.append((loser_name, winner_name))
+    node_to_name_map = {}
+    name_to_node_map = {}
+    i=0
+    G = snap.PNGraph.New()
+    for loser, winner in match_win_list:
+        if loser not in name_to_node_map:
+            nid = G.AddNode(-1)
+            node_to_name_map[nid] = loser
+            name_to_node_map[loser] = nid
+        if winner not in name_to_node_map:
+            nid = G.AddNode(-1)
+            node_to_name_map[nid] = winner
+            name_to_node_map[winner] = nid
+        G.AddEdge(name_to_node_map[loser], name_to_node_map[winner])
+    return G
 
 #createCompetitionGraph(seeding, results)
